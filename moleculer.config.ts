b772bcb676bd2d9f7aaab8fbe5dce389227ffcd8
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2023-10-30 08:35:37 +0200

    Actions setup + cleanup

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -6,8 +6,6 @@ import {
   ServiceBroker,
 } from 'moleculer';
 
-require('@moleculer/lab');
-
 /**
  * Moleculer ServiceBroker configuration file
  *
@@ -43,26 +41,21 @@ const brokerConfig: BrokerOptions = {
 
   // Enable/disable logging or use custom logger. More info: https://moleculer.services/docs/0.14/logging.html
   // Available logger types: "Console", "File", "Pino", "Winston", "Bunyan", "debug", "Log4js", "Datadog"
-  logger: [
-    {
-      type: 'Console',
-      options: {
-        // Using colors on the output
-        colors: true,
-        // Print module names with different colors (like docker-compose for containers)
-        moduleColors: false,
-        // Line formatter. It can be "json", "short", "simple", "full", a `Function` or a template string like "{timestamp} {level} {nodeID}/{mod}: {msg}"
-        formatter: 'full',
-        // Custom object printer. If not defined, it uses the `util.inspect` method.
-        objectPrinter: null,
-        // Auto-padding the module name in order to messages begin at the same column.
-        autoPadding: false,
-      },
+  logger: {
+    type: 'Console',
+    options: {
+      // Using colors on the output
+      colors: true,
+      // Print module names with different colors (like docker-compose for containers)
+      moduleColors: false,
+      // Line formatter. It can be "json", "short", "simple", "full", a `Function` or a template string like "{timestamp} {level} {nodeID}/{mod}: {msg}"
+      formatter: 'full',
+      // Custom object printer. If not defined, it uses the `util.inspect` method.
+      objectPrinter: null,
+      // Auto-padding the module name in order to messages begin at the same column.
+      autoPadding: false,
     },
-    {
-      type: 'Laboratory',
-    },
-  ],
+  },
   // Default log level for built-in console logger. It can be overwritten in logger options above.
   // Available values: trace, debug, info, warn, error, fatal
   logLevel: 'info',
@@ -79,7 +72,7 @@ const brokerConfig: BrokerOptions = {
     type: 'Redis',
     options: {
       redis: process.env.REDIS_CONNECTION,
-      prefix: 'zvejyba',
+      prefix: 'biip-zvejyba',
       ttl: 60 * 60, // 1 hour
     },
   },
@@ -189,7 +182,6 @@ const brokerConfig: BrokerOptions = {
           }),
         },
       },
-      'Laboratory',
     ],
   },
 
@@ -219,7 +211,7 @@ const brokerConfig: BrokerOptions = {
           defaultTags: null,
         },
       },
-      process.env.NODE_ENV !== 'production'
+      process.env.NODE_ENV === 'local'
         ? {
             type: 'Console', // Console exporter is only for development!
             options: {
