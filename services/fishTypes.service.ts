dd2606698336fcc53d7322cec933fa7cf9de4294
LWangllix <vanglikas.lukas@gmail.com>
2023-10-30 21:35:15 +0200

    minio fix

--- services/fishTypes.service.ts
+++ services/fishTypes.service.ts
@@ -83,7 +83,7 @@ export default class FishTypesService extends moleculer.Service {
 
     return ctx.call('minio.uploadFile', {
       payload: ctx.params,
-      isPrivate: false,
+      isPrivate: true,
       types: IMAGE_TYPES,
       folder,
     });
