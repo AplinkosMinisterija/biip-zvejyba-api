0c1d362527a36676e308c1169b78cd5d05e2a149
LWangllix <vanglikas.lukas@gmail.com>
2023-11-03 00:50:03 +0200

    remove console log

--- utils/functions.ts
+++ utils/functions.ts
@@ -3,19 +3,12 @@ import { AuthUserRole, UserAuthMeta } from '../services/api.service';
 import { TenantUserRole } from '../services/tenantUsers.service';
 import { throwNoRightsError } from '../types';
 
-export const validateCanEditTenantUser = (
-  ctx: Context<any, UserAuthMeta>,
-  err: string
-) => {
+export const validateCanEditTenantUser = (ctx: Context<any, UserAuthMeta>, err: string) => {
   const { profile } = ctx.meta;
-
-  console.log(ctx.meta.authUser, ' ctx.meta.authUser');
 
   if (
     ctx.meta.authUser?.type === AuthUserRole.USER &&
-    ![TenantUserRole.OWNER, TenantUserRole.USER_ADMIN].includes(
-      ctx.meta.user.tenants[profile]
-    )
+    ![TenantUserRole.OWNER, TenantUserRole.USER_ADMIN].includes(ctx.meta.user.tenants[profile])
   ) {
     throwNoRightsError(err);
   }
