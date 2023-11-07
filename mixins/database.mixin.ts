45b6f4edd130a6a42d82ff68aec0ac3927a0bdd7
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2023-11-07 15:18:49 +0200

    improved database mixin

--- mixins/database.mixin.ts
+++ mixins/database.mixin.ts
@@ -1,9 +1,74 @@
 'use strict';
 
 import _ from 'lodash';
+import { Context } from 'moleculer';
 import filtersMixin from 'moleculer-knex-filters';
 const DbService = require('@moleculer/database').Service;
 const knex = require('../knexfile');
+
+export function PopulateHandlerFn(action: string) {
+  return async function (
+    ctx: Context<{ populate: string | string[] }>,
+    values: any[],
+    docs: any[],
+    field: any,
+  ) {
+    if (!values.length) return null;
+    const rule = field.populate;
+    let populate = rule.params?.populate;
+    if (rule.inheritPopulate) {
+      populate = ctx.params.populate;
+    }
+    const params = {
+      ...(rule.params || {}),
+      id: values,
+      mapping: true,
+      populate,
+      throwIfNotExist: false,
+    };
+
+    const byKey: any = await ctx.call(action, params, rule.callOptions);
+
+    let fieldName = field.name;
+    if (rule.keyField) {
+      fieldName = rule.keyField;
+    }
+
+    return docs?.map((d) => {
+      const fieldValue = d[fieldName];
+      if (!fieldValue) return null;
+      return byKey[fieldValue] || null;
+    });
+  };
+}
+
+function makeMapping(
+  data: any[],
+  mapping?: string,
+  options?: {
+    mappingMulti?: boolean;
+    mappingField?: string;
+  },
+) {
+  if (!mapping) return data;
+
+  return data?.reduce((acc: any, item) => {
+    let value: any = item;
+
+    if (options?.mappingField) {
+      value = item[options.mappingField];
+    }
+
+    if (options?.mappingMulti) {
+      return {
+        ...acc,
+        [`${item[mapping]}`]: [...(acc[`${item[mapping]}`] || []), value],
+      };
+    }
+
+    return { ...acc, [`${item[mapping]}`]: value };
+  }, {});
+}
 
 export default function (opts: any = {}) {
   const adapter: any = {
@@ -14,24 +79,105 @@ export default function (opts: any = {}) {
     },
   };
 
-  opts = _.defaultsDeep(opts, { adapter, createActions: { replace: false } }, { cache: false });
+  const cache = {
+    enabled: false,
+  };
+
+  opts = _.defaultsDeep(opts, { adapter }, { cache: opts.cache || cache });
+
+  const removeRestActions: any = {};
+
+  if (opts?.createActions === undefined || opts?.createActions !== false) {
+    removeRestActions.replace = {
+      rest: null as any,
+    };
+  }
 
   const schema = {
     mixins: [DbService(opts), filtersMixin()],
 
     actions: {
+      ...removeRestActions,
+
       findOne(ctx: any) {
         return this.findEntity(ctx);
+      },
+
+      async removeAllEntities(ctx: any) {
+        return await this.clearEntities(ctx);
+      },
+
+      async populateByProp(
+        ctx: Context<{
+          id: number | number[];
+          queryKey: string;
+          query: any;
+          mapping?: boolean;
+          mappingMulti?: boolean;
+          mappingField: string;
+        }>,
+      ): Promise<any> {
+        const { queryKey, query, mapping, mappingMulti, mappingField } = ctx.params;
+
+        const ids = Array.isArray(ctx.params.id) ? ctx.params.id : [ctx.params.id];
+
+        delete ctx.params.queryKey;
+        delete ctx.params.id;
+        delete ctx.params.mapping;
+        delete ctx.params.mappingMulti;
+        delete ctx.params.mappingField;
+
+        const entities = await this.findEntities(ctx, {
+          ...ctx.params,
+          query: {
+            ...(query || {}),
+            [queryKey]: { $in: ids },
+          },
+        });
+
+        const resultById = makeMapping(entities, mapping ? queryKey : '', {
+          mappingMulti,
+          mappingField: mappingField,
+        });
+
+        return ids.reduce(
+          (acc: any, id) => ({
+            ...acc,
+            [`${id}`]: resultById[id] || (mappingMulti ? [] : ''),
+          }),
+          {},
+        );
       },
     },
 
     methods: {
-      filterQueryIds(ids: Array<number>, queryIds?: any) {
+      filterQueryIds(ids: number[], queryIds?: any) {
         if (!queryIds) return ids;
 
         queryIds = (Array.isArray(queryIds) ? queryIds : [queryIds]).map((id: any) => parseInt(id));
 
-        return ids.filter((id: number) => queryIds.indexOf(id) >= 0);
+        return ids.filter((id) => queryIds.indexOf(id) >= 0);
+      },
+    },
+
+    hooks: {
+      after: {
+        find: [
+          async function (
+            ctx: Context<{
+              mapping: string;
+              mappingMulti: boolean;
+              mappingField: string;
+            }>,
+            data: any[],
+          ) {
+            const { mapping, mappingMulti, mappingField } = ctx.params;
+            return makeMapping(data, mapping, {
+              mappingMulti,
+              mappingField,
+            });
+          },
+        ],
       },
     },
 
